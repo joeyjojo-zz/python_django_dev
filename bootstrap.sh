6e7c04daa6d067d9875d6dad8f08b585308ced32
Jon Dunleavy <jon.dunleavy@pennantplc.co.uk>
2013-06-10 14:58:04 +0100

    Updated bootstrap so that it updates to the latest version of chef

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,4 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y git
-apt-get install -y python-pip
-apt-get install -y nginx
-apt-get install -y postgresql
-apt-get install -y curl
-
-pip install gunicorn
-pip install django
-pip install django-tastypie
-pip install psycopg2
-
-sudo service nginx stop
-sudo nginx -c /vagrant/django_gunicorn_nginx.conf
-sudo service nginx start
-sudo python /vagrant/src/manage.py runserver 127.0.0.1:8000
-# sudo python /vagrant/src/manage.py run_gunicorn 127.0.0.1:8000
+sudo apt-get install -y curl
+curl -L https://www.opscode.com/chef/install.sh | sudo bash
