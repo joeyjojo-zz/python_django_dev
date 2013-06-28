a65fac716d09e0d2a4cbd3d3ab39b45a337b63e1
Jon Dunleavy <joeyjojosmith@gmail.com>
2013-06-28 17:42:58 +0100

    Updated settings

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         'NAME': 'django_db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'django_login',
-        'PASSWORD': 'your_password',
+        'PASSWORD': 'password',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'polls',
 )
 
 # A sample logging configuration. The only tangible logging
